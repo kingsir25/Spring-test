136dc2312302f4f2aeefe6a4713c1dac1299a171
kingsir25 <41620994@qq.com>
2017-08-27 13:49:01 +0800

    update pom.xml helloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/hi")
 public class HelloController {
     @Autowired
     private GirlProperties girlProperties;
